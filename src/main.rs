fe0ab798379d015c5eaff52792308452fdba5eb5
razvan mbp <raz-mbpz@localhost>
2024-03-20 19:44:49 +0200

    added config crate + configuration module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 
 use std::net::TcpListener;
-
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("failed to bind");
+    let configuration = get_configuration().expect("failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("failed to bind");
     run(listener)?.await
 }
