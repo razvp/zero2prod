d4dea46736ce2f9b9bbc2f29f4c1287b208da812
razvan mbp <raz-mbpz@localhost>
2024-03-20 20:00:58 +0200

    added .env so sqlx has DATABASE_URL at compile time

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,8 @@
 use std::{collections::HashMap, net::TcpListener, vec};
 
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configuration;
+
 #[tokio::test]
 async fn health_check_works() {
     let app_address = spawn_app();
@@ -18,10 +21,16 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("failed to read config");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("failed to connect to DB");
+
     let client = reqwest::Client::new();
     let mut params = HashMap::new();
     params.insert("name", "le guin");
-    params.insert("email", "test@email.com");
+    params.insert("email", "ursula_le_guin@gmail.com");
 
     let response = client
         .post(&format!("{app_address}/subscribe"))
@@ -30,6 +39,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .await
         .expect("failed to execute request");
     assert_eq!(200, response.status());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("failed to fetch subscription");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
