a8b7ee206eec0e41fa3b1fcf497824ad29344d8a
razvan mbp <raz-mbpz@localhost>
2024-03-21 21:25:06 +0200

    subscribe route works

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,10 @@
-
-use actix_web::{post, web, HttpResponse};
+use crate::startup::State;
+use actix_web::{post, web, HttpRequest, HttpResponse};
 use serde::Deserialize;
+use sqlx::types::chrono::Local;
+use sqlx::types::Uuid;
+use sqlx::Connection;
+use sqlx::PgConnection;
 
 #[derive(Deserialize, Debug)]
 struct FormData {
@@ -8,9 +12,32 @@ struct FormData {
     email: String,
 }
 
-
 #[post("/subscribe")]
-async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
-    dbg!(&form);
-    HttpResponse::Ok().finish()
+async fn subscribe(req: HttpRequest, form: web::Form<FormData>) -> HttpResponse {
+    let connection_string = &req.app_data::<State>().unwrap().connection_string;
+    let mut connection = PgConnection::connect(connection_string)
+        .await
+        .expect("failed to connect to DB");
+    let name = &form.name;
+    let email = &form.email;
+    let subscribed_at = Local::now();
+    let id = uuid::Uuid::new_v4();
+
+    let insert = sqlx::query!(
+        "INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)",
+        id,
+        email,
+        name,
+        subscribed_at
+    )
+    .execute(&mut connection)
+    .await;
+
+    match insert {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("error: {e:?}");
+            HttpResponse::BadRequest().body("account exists")
+        }
+    }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -2,12 +2,22 @@
 use std::net::TcpListener;
 use actix_web::{dev::Server, App, HttpServer};
 
-use crate::routes::{health_check,subscribe};
+use crate::{configuration::get_configuration, routes::{health_check,subscribe}};
+
+#[derive(Clone)]
+pub(crate) struct State {
+    pub(crate) connection_string: String,
+}
+
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     println!("test");
-    let server = HttpServer::new(|| {
-        App::new()
+    let configuration = get_configuration().expect("failed to read config");
+    let connection_string = configuration.database.connection_string();
+
+    let server = HttpServer::new(move || {
+        let state = State {connection_string : connection_string.clone()};
+        App::new().app_data(state)
             .service(health_check)
             .service(subscribe)
     })
