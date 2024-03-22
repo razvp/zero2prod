64af69c11cc9f561bf80a0761457a90ac7f7b09b
razvan mbp <raz-mbpz@localhost>
2024-03-22 15:12:46 +0200

    test suite creates a new db for every test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,55 @@
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::{collections::HashMap, net::TcpListener, vec};
+use uuid::Uuid;
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
 
-use sqlx::{Connection, PgConnection};
-use zero2prod::configuration::get_configuration;
+struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+async fn spawn_app() -> TestApp {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind");
+    let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127.0.0.1:{port}");
+    let mut configuration = get_configuration().expect("failed to read config in test spawn_app()");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let connection_pool = configure_database(&configuration.database).await;
+
+    let server =
+        zero2prod::startup::run(listener, connection_pool.clone()).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
+}
+
+async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("failed to connect to postgres");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("failed to create database");
+
+    // Migrate database
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+    .expect("failed to connect to postgres");
+    sqlx::migrate!("./migrations")
+    .run(&connection_pool).await.expect("failed to migrate db");
+
+    connection_pool
+}
 
 #[tokio::test]
 async fn health_check_works() {
-    let app_address = spawn_app();
+    let test_app = spawn_app().await;
+    let app_address = test_app.address;
     let client = reqwest::Client::new();
 
     let response = client
@@ -20,20 +64,14 @@ async fn health_check_works() {
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    let app_address = spawn_app();
-    let configuration = get_configuration().expect("failed to read config");
-    let connection_string = configuration.database.connection_string();
-    let mut connection = PgConnection::connect(&connection_string)
-        .await
-        .expect("failed to connect to DB");
-
+    let test_app = spawn_app().await;
     let client = reqwest::Client::new();
+
     let mut params = HashMap::new();
     params.insert("name", "le guin");
     params.insert("email", "ursula_le_guin@gmail.com");
-
     let response = client
-        .post(&format!("{app_address}/subscribe"))
+        .post(&format!("{}/subscribe", test_app.address))
         .form(&params)
         .send()
         .await
@@ -41,7 +79,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions")
-        .fetch_one(&mut connection)
+        .fetch_one(&test_app.db_pool)
         .await
         .expect("failed to fetch subscription");
 
@@ -51,7 +89,8 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
-    let app_address = spawn_app();
+    let test_app = spawn_app().await;
+    let app_address = test_app.address;
     let client = reqwest::Client::new();
 
     let test_cases = vec![
@@ -75,13 +114,4 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
             error_message
         );
     }
-}
-
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::startup::run(listener).expect("Failed to bint address");
-    let _ = tokio::spawn(server);
-
-    format!("http://127.0.0.1:{port}")
 }
