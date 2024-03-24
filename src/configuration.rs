d5b675a9abc423acca34aeec498d793fec22703a
razvan mbp <raz-mbpz@localhost>
2024-03-24 19:31:26 +0200

    add ability to override any opt with env vars
    also force serde to deserialize to number, because ENV vars are strings

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,5 @@
-use config::Environment;
 use secrecy::{ExposeSecret, SecretString};
+use serde_aux::field_attributes::deserialize_number_from_string;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -9,6 +9,7 @@ pub struct Settings {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -17,6 +18,7 @@ pub struct ApplicationSettings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: SecretString,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
@@ -48,15 +50,23 @@ impl DatabaseSettings {
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let base_path = std::env::current_dir().expect("failed to detect current directory");
     let configuration_directory = base_path.join("configuration");
-    let environment = std::env::var("APP_ENVIRONMENT")
-        .unwrap_or_else(|_| "local".into());
+    let environment = std::env::var("APP_ENVIRONMENT").unwrap_or_else(|_| "local".into());
     let environment_filename = format!("{}.yaml", environment);
 
     let settings = config::Config::builder()
+        .add_source(config::File::from(
+            configuration_directory.join("base.yaml"),
+        ))
+        .add_source(config::File::from(
+            configuration_directory.join(environment_filename),
+        ))
+        // Add in settings from env vars (with a prefix APP and __ as separator
+        // ex: APP_APPLICATION__PORT=5001 would se `Settings.application.port`
+        // this allows to override any variable (even if set in config file)
         .add_source(
-            config::File::from(configuration_directory.join("base.yaml"))
-        ).add_source(
-            config::File::from(configuration_directory.join(environment_filename))
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__")
         )
         .build()?;
     settings.try_deserialize::<Settings>()
