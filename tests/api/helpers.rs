19071e88073ef1a8c312c26927db7d3047a149c8
razvan mbp <raz-mbpz@localhost>
2024-04-02 21:39:53 +0300

    refactoring tests

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -10,6 +10,11 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 static TRACING: Once = Once::new();
 
+pub struct ConfirmationLinks {
+    pub html: reqwest::Url,
+    pub plain_text: reqwest::Url,
+}
+
 pub struct TestApp {
     pub address: String,
     pub port: u16,
@@ -26,6 +31,28 @@ impl TestApp {
             .send()
             .await
             .expect("failed to execute request")
+    }
+
+    pub fn get_confirmation_links(&self, email_request: &wiremock::Request) -> ConfirmationLinks {
+        let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+        let get_link = |s: &str| {
+            let links: Vec<_> = linkify::LinkFinder::new()
+                .links(s)
+                .filter(|l| *l.kind() == linkify::LinkKind::Url)
+                .collect();
+            assert_eq!(links.len(), 1);
+            let raw_link = links[0].as_str().to_owned();
+            let mut confirmation_link = reqwest::Url::parse(&raw_link).unwrap();
+            // makse sure we don't call random APIs on the web
+            assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
+            confirmation_link.set_port(Some(self.port)).unwrap();
+            confirmation_link
+        };
+
+        let html_link = get_link(&body["HtmlBody"].as_str().unwrap());
+        let text_link = get_link(&body["TextBody"].as_str().unwrap());
+
+        ConfirmationLinks { html: html_link, plain_text: text_link}
     }
 }
 
