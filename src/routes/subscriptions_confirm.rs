568cde178a75c85756ee10d0d64a4238afca4a85
razvan mbp <raz-mbpz@localhost>
2024-04-02 19:16:17 +0300

    test: confirmations_without_token_are_rejected_with_a_400

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,8 +1,20 @@
-use actix_web::{get, HttpResponse};
+use actix_web::{get, web, HttpResponse};
+use serde::Deserialize;
+
+
+#[derive(Debug, Deserialize)]
+struct Parameters {
+    subscription_token: String,
+}
 
 
 
+#[tracing::instrument(
+    name = "Confirm a pending subscriber",
+    skip(_parameters)
+)]
 #[get("/subscriptions/confirm")]
-pub async fn confirm() -> HttpResponse {
+pub async fn confirm(_parameters: web::Query<Parameters>) -> HttpResponse {
+    dbg!(&_parameters);
     HttpResponse::Ok().finish()
 }
