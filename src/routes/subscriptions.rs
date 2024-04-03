1b089f504bcd5c8f45f976199ba595b5d8890f22
razvan mbp <raz-mbpz@localhost>
2024-04-03 19:40:23 +0300

    removed extra error logs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -163,10 +163,7 @@ pub async fn insert_subscriber(
         new_subscriber.name.as_ref(),
         Utc::now()
     );
-    transaction.execute(query).await.map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
+    transaction.execute(query).await?;
     Ok(subscriber_id)
 }
 
@@ -193,10 +190,7 @@ pub async fn store_token(
         subscription_token,
         subscriber_id
     );
-    transaction.execute(query).await.map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        StoreTokenError(e)
-    })?;
+    transaction.execute(query).await.map_err(StoreTokenError)?;
 
     Ok(())
 }
