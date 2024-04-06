5e6363c241e7e46d8074e3249949775347c3237a
razvan mbp <raz-mbpz@localhost>
2024-04-06 10:16:49 +0300

    added password auth skeleton (random credentials)

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,6 +1,11 @@
-use actix_web::{post, web, HttpResponse, ResponseError};
+use actix_web::{
+    http::{header::HeaderMap, header::HeaderValue, header},
+    post, web, HttpRequest, HttpResponse, ResponseError,
+};
 use anyhow::Context;
+use base64::Engine;
 use reqwest::StatusCode;
+use secrecy::SecretString;
 use sqlx::PgPool;
 use tracing::instrument;
 
@@ -8,6 +13,8 @@ use crate::{domain::SubscriberEmail, email_client::EmailClient, routes::error_ch
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
     #[error(transparent)]
     UnexpectedError(#[from] anyhow::Error),
 }
@@ -30,24 +37,56 @@ pub struct Content {
     text: String,
 }
 
-// this needs to send emails to all subscribers that have a `confirmed` status
-// 0. get newsletter issue details from incoming API call
-// 1. get all subscribers with `confirmed` status
-// 2. send email for each one, or maybe bulk?
+struct Credentials {
+    username: String,
+    password: SecretString,
+}
+
+fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
+    let header_value = headers
+        .get("Authorization")
+        .context("The 'Authorization' header was missing")?
+        .to_str()
+        .context("The 'Authorization' header was not a valid UTF8 string.")?;
+    let base64encoded_segment = header_value
+        .strip_prefix("Basic ")
+        .context("The authorization scheme was not 'Basic'.")?;
+    let decoded_bytes = base64::engine::general_purpose::STANDARD
+        .decode(base64encoded_segment)
+        .context("Failed to base64-decode 'Basic' credentials.")?;
+    let decoded_credentials = String::from_utf8(decoded_bytes)
+        .context("The decoded credential string is not valid UTF8")?;
+
+    let mut credentials = decoded_credentials.splitn(2, ":");
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth."))?
+        .to_string();
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth."))?
+        .to_string();
+
+    Ok(Credentials { username , password: SecretString::new(password) })
+}
+
 #[post("/newsletters")]
 pub async fn publish_newsletter(
     body: web::Json<BodyData>,
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
+    request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
-    let _subscribers = get_confirmed_subscribers(&pool).await?;
-    for subscriber in _subscribers {
+    let _credentials = basic_authentication(request.headers())
+        .map_err(PublishError::AuthError)?;
+    let subscribers = get_confirmed_subscribers(&pool).await?;
+    for subscriber in subscribers {
         match subscriber {
             Ok(subscriber) => {
                 email_client
                     .send_email(
                         // clone here so we use it in .with_context()
-                        &subscriber.email, 
+                        &subscriber.email,
                         &body.title,
                         &body.content.html,
                         &body.content.text,
@@ -56,8 +95,7 @@ pub async fn publish_newsletter(
                     .with_context(|| {
                         format!("Failed to send newsletter isssue to: {}", subscriber.email)
                     })?;
-
-            },
+            }
             Err(error) => {
                 tracing::warn!(
                     // record error chain as structured field
@@ -65,16 +103,25 @@ pub async fn publish_newsletter(
                     "Skipping a confirmed subscriber. \
                     Their stored contact details are invalid",
                 );
-            },
-        }}
+            }
+        }
+    }
 
     Ok(HttpResponse::Ok().finish())
 }
 
 impl ResponseError for PublishError {
-    fn status_code(&self) -> reqwest::StatusCode {
+    fn error_response(&self) -> HttpResponse {
         match self {
-            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            PublishError::UnexpectedError(_) => HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR),
+            PublishError::AuthError(_) => {
+                let mut response = HttpResponse::new(StatusCode::UNAUTHORIZED);
+                let header_value = HeaderValue::from_str(r#"Basic realm="publish""#)
+                    .unwrap();
+                response.headers_mut().insert(header::WWW_AUTHENTICATE, header_value);
+
+                response
+            },
         }
     }
 }
@@ -87,18 +134,17 @@ struct ConfirmedSubscriber {
 async fn get_confirmed_subscribers(
     pool: &PgPool,
 ) -> Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
-    let rows = sqlx::query!(
-        "SELECT email FROM subscriptions WHERE status = 'confirmed'"
-    )
-    .fetch_all(pool)
-    .await?;
+    let rows = sqlx::query!("SELECT email FROM subscriptions WHERE status = 'confirmed'")
+        .fetch_all(pool)
+        .await?;
 
-    let confirmed_subscribers =
-        rows.into_iter()
-            .map(|r| match SubscriberEmail::parse(r.email) {
-                Ok(email) => Ok(ConfirmedSubscriber { email }),
-                Err(error) => Err(anyhow::anyhow!(error)),
-            }).collect();
+    let confirmed_subscribers = rows
+        .into_iter()
+        .map(|r| match SubscriberEmail::parse(r.email) {
+            Ok(email) => Ok(ConfirmedSubscriber { email }),
+            Err(error) => Err(anyhow::anyhow!(error)),
+        })
+        .collect();
 
     Ok(confirmed_subscribers)
 }
--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -2,6 +2,34 @@ use crate::helpers::{spawn_app, ConfirmationLinks, TestApp};
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
 
+
+#[tokio::test]
+async fn requests_missing_authorization_are_rejected() {
+    // Arrange
+    let app = spawn_app().await;
+    // Act
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>",
+            }
+        }))
+        .send()
+        .await
+        .expect("failed to execute request");
+
+
+    
+
+    // Assert
+    assert_eq!(401, response.status());
+    assert_eq!(r#"Basic realm="publish""#, response.headers()["WWW-Authenticate"]);
+}
+
+
 #[tokio::test]
 async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
     // Arrange
