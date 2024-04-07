778baf11bfa80b254fe223495c8220fd993819ec
razvan mbp <raz-mbpz@localhost>
2024-04-07 17:18:41 +0300

    User enumeration attack prevention

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -167,17 +167,33 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let (user_id, expected_password_hash) = get_stored_credentials(&credentials.username, &pool)
+    let mut user_id = None;
+    // We use this to perform the same amout of work even if the
+    // user doesn't exist so we mitigate a User enumeration attack
+    let mut expected_password_hash = SecretString::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$\
+        gZiV/M1gPc22ElAH/Jh1Hw$\
+        CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno".to_string()
+    );
+
+    if let Some((stored_user_id, stored_password_hash)) = get_stored_credentials(&credentials.username, &pool)
         .await
         .map_err(PublishError::UnexpectedError)?
-        .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))?;
+    {
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    }
+
 
     spawn_blocking_with_tracing(move || verify_password_hash(expected_password_hash, credentials.password))
     .await
     .context("Failed to spawn blocking task")
     .map_err(PublishError::UnexpectedError)??;
 
-    Ok(user_id)
+    // Will be Some only if password matches,
+    // eliminating the slight risk if password provided matches
+    // but user is inexistent
+    user_id.ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))
 }
 
 #[tracing::instrument(
